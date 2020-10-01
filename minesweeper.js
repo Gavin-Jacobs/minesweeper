356636a3b02dadd20dc3ee935575cdd80981937a
Gavin-Jacobs <gavin@phoenixvir.net>
2020-10-02 00:27:09 +1300

    now

--- minesweeper.js
+++ minesweeper.js
@@ -87,7 +87,7 @@ function startGame () {
 // 2. Are all of the mines marked?
 function checkForWin () {
   for( i=0; i<board.cells.length; i++) {
-    if ((board['cells'][0,1,2,3,4,5,6,7,8].isMine==true &&board['cells'][0,1,2,3,4,5,6,7,8].isMarked==true)==true &&(board['cells'][0,1,2,3,4,5,6,7,8].isMine==false&&board['cells'][0,1,2,3,4,5,6,7,8].hidden==false)==true)
+    if ((board.cells[0,1,2,3,4,5,6,7,8].isMine==true &&board.cells[0,1,2,3,4,5,6,7,8].isMarked==true)==true &&(board.cells[0,1,2,3,4,5,6,7,8].isMine==false&&board.cells[0,1,2,3,4,5,6,7,8].hidden==false)==true)
      lib.displayMessage('You win!')
   } 
  
