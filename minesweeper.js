06128cf070db53101879485f6d5e76b745ec176c
Gavin-Jacobs <gavin@phoenixvir.net>
2020-10-01 22:43:26 +1300

    try

--- minesweeper.js
+++ minesweeper.js
@@ -75,10 +75,10 @@ function startGame () {
   lib.initBoard()
   var newNum=0
   for(i=0; i<board['cells'][0,1,2,3,4,5,6,7,8].length;i++){
-    countSurroundingMines(board.cells[0,1,2,3,4,5,6,7,8])= board.cells[0,1,2,3,4,5,6,7,8].surroundingMines
+    newNum=countSurroundingMines(board.cells[0,1,2,3,4,5,6,7,8])= board.cells[0,1,2,3,4,5,6,7,8].surroundingMines
       
   }
- 
+ board['cells'][0,1,2,3,4,5,6,7,8].surroundingMines=newNum
 }
 // Define this function to look for a win condition:
 //
