db3e07de97cbb1466703c270e4848f190ffc2245
Gavin-Jacobs <gavin@phoenixvir.net>
2020-10-01 22:29:11 +1300

    ?

--- minesweeper.js
+++ minesweeper.js
@@ -74,7 +74,7 @@ function startGame () {
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
   var newNum=0
-  for(i=0; i<board.cells.length;i++){
+  for(i=0; i<board['cells'][0,1,2,3,4,5,6,7,8].length;i++){
     countSurroundingMines(board.cells[0,1,2,3,4,5,6,7,8])
   }
   board.cells[0,1,2,3,4,5,6,7,8].surroundingMines=newNum
