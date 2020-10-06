5518025fb02936a9e9b114977a6af4c8a314b24d
Gavin-Jacobs <gavin@phoenixvir.net>
2020-10-06 15:10:08 +1300

    try

--- minesweeper.js
+++ minesweeper.js
@@ -75,7 +75,7 @@ function startGame () {
   lib.initBoard()
   
   for(i=0;i<board['cells'].length;i++){
-    board['cells'].surroundingMines=countSurroundingMines(board['cells'][0,1,2,3,4,5,6,7,8])
+   countSurroundingMines(board['cells'][0,1,2,3,4,5,6,7,8])=board.cells[0,1,2,3,4,5,6,7,8].surroundingMines
   }
   document.addEventListener('contextmenu',checkForWin)
 }
