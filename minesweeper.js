097ec4cedfc5584ed4578229d4bf0db8c8df58b4
Gavin-Jacobs <gavin@phoenixvir.net>
2020-10-08 13:41:29 +1300

    try

--- minesweeper.js
+++ minesweeper.js
@@ -116,7 +116,7 @@ function countSurroundingMines (cell) {
     count++;
     
     }
-    
+    return count
   }
   //console.log(surrounding.length)
   board.cells[0,1,2,3,4,5,6,7,8].surroundingMines=count
