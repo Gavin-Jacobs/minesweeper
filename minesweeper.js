47cd711c3d779ab2420b95c34fde397e2ded1978
Gavin-Jacobs <gavin@phoenixvir.net>
2020-10-08 14:37:48 +1300

    win test

--- minesweeper.js
+++ minesweeper.js
@@ -88,17 +88,30 @@ function startGame () {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
+  var count1=0
+  var count2=0
+  for(i=0;i<board.cells.length;i++){
+    if(board.cells[i].isMine==true){
+      count1++
+    }
+    if(board.cell[i].isMine==false){
+      count2++
+    }
+  }
+  var count3=0
+  var count4=0
   for( i=0; i<board.cells.length; i++) {
     if (board.cells[0,1,2,3,4,5,6,7,8].isMine==true&&board.cells[0,1,2,3,4,5,6,7,8].isMarked==true){
     
-    console.log('yes')
-    return lib.displayMessage('You win!')
+    count3++
     }
     if(board.cells[i].isMine==false&&board.cells[i].hidden==false){
-      console.log('no')
+      count4++
     }
   }
-  
+  if(count1==count3&&count2==count4){
+    console.log('whynot')
+  }
     
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
